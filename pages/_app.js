050c1021dff83a3438b880c4b77995719c11e1bd
prosperor <prospero@edu.unifil.br>
2020-03-22 18:03:19 -0200

    subindo

---
+++ pages/_app.js
@@ -0,0 +1,54 @@
+import React, {
+    useEffect
+} from 'react';
+
+import Head from 'next/head';
+
+import {
+    ThemeProvider
+} from '@material-ui/core/styles';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import PropTypes from 'prop-types';
+import theme from '../components/theme';
+
+//Essas barras "/" funcionam como um chave de fechamento de cada chave
+//Theme Provider obtem o tema personalizado para a interface do usuario do material
+
+function MyApp({
+    Component,
+    pageProps
+}) {
+    useEffect(
+        () => {
+            const jssStyles = document.querySelector('#jss-server-side');
+            if (jssStyles) {
+                jssStyles.parentElement.removeChild(jssStyles);
+            }
+        }, []);
+    return (
+        <React.Fragment>
+            <head>
+            
+            <title> Portifólio </title>
+            
+            <meta name="viewport"
+                content="minimum-scale=1,
+                        initial-scale=1, width=device-width"/>
+
+            </head>
+
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Component {...pageProps}/>
+            </ThemeProvider>
+        </React.Fragment>
+    );
+}
+
+MyApp.prototype={
+    Component: PropTypes.oneOfType([PropTypes.node,PropTypes.func]),
+    pageProps: PropTypes.object
+}
+
+export default MyApp;
---
+++ pages/theme.js
@@ -0,0 +1,14 @@
+import { creatMuiTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+
+//Criando o Tema
+
+const theme = creatMuiTheme(
+    {
+        palette: {
+            primary: {
+                main: '#283E4A',
+            },
+        }
+    }
+)
