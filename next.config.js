db6f77ce79fe37400e7da631d460f03764b95d90
prosperor <prospero@edu.unifil.br>
2020-03-23 19:14:56 -0200

    apagando

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const withMDX = require('@next/mdx')({
-    extension: /\.mdx?$/
-});
-
-module.exports = withMDX({
-    pageExtensions: ['js', 'jsx', 'md', 'mdx']
-});
-
-//Isso diz para importar arquivos com extensão .md(markdown) automaticamente e processá-los como arquivos JSX, veremos que nossa postagem é na verdade os dois, um componente JSX React e um arquivo Markdown ao mesmo tempo.
