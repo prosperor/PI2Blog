bf952c50fea3ce5a2cfe963a7065996550a6dea0
prosperor <prospero@edu.unifil.br>
2020-05-04 18:29:28 -0300

    mudanças

--- themes/terminal/postcss.config.js
+++ themes/terminal/postcss.config.js
@@ -1,11 +1,11 @@
 const url = require("postcss-url");
 const imports = require("postcss-import");
 const nested = require("postcss-nested");
-const postCSSPresetEnv = require("./node_modules/postcss-preset-env");
+const postCSSPresetEnv = require("postcss-preset-env");
 const browsers = require("browserslist");
 const cssnano = require("cssnano");
-const color = require("./node_modules/postcss-color-mod-function");
-const mixins = require("./node_modules/postcss-mixins");
+const color = require("postcss-color-mod-function");
+const mixins = require("postcss-mixins");
 
 module.exports = () => ({
   plugins: [
